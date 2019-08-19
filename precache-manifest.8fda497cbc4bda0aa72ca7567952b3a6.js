ba6b7309e34dc51b8a746fd7604afa29c053f4d2
cruzach <charliecruzan@gmail.com>
2019-08-19 11:15:31 -0400

    Updates

---
+++ precache-manifest.8fda497cbc4bda0aa72ca7567952b3a6.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/expo-gh-pages-test/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "53f69df5c408d1a2b195",
+    "url": "/expo-gh-pages-test/static/js/app.91325d3b.chunk.js"
+  },
+  {
+    "revision": "95c7aa884000e3b06060",
+    "url": "/expo-gh-pages-test/static/js/runtime~app.1d42a89d.js"
+  },
+  {
+    "revision": "6165c9d7a2e729ba57b23dd93add5366",
+    "url": "/expo-gh-pages-test/static/media/back-icon-mask.6165c9d7.png"
+  },
+  {
+    "revision": "c7578911196974808febf223f05a8364",
+    "url": "/expo-gh-pages-test/static/media/robot-prod.c7578911.png"
+  },
+  {
+    "revision": "54da1e9816c77e30ebc5920e256736f2",
+    "url": "/expo-gh-pages-test/static/media/robot-dev.54da1e98.png"
+  },
+  {
+    "revision": "761a29cc5b9163dd2a23118c3d85605d",
+    "url": "/expo-gh-pages-test/static/media/expo-icon.761a29cc.png"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/expo-gh-pages-test/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "3d677c3c4ad3cd32f3b1dc291d46b55e",
+    "url": "/expo-gh-pages-test/index.html"
+  },
+  {
+    "revision": "11ac42bde3327b9bcaae8371565d0e36",
+    "url": "/expo-gh-pages-test/manifest.json"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/expo-gh-pages-test/serve.json"
+  },
+  {
+    "revision": "37af9a2653c9f58a47e7",
+    "url": "/expo-gh-pages-test/static/js/2.58364428.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/expo-gh-pages-test/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/expo-gh-pages-test/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/expo-gh-pages-test/favicon.ico"
+  },
+  {
+    "revision": "49a79d66bdea2debf1832bf4d7aca127",
+    "url": "/expo-gh-pages-test/./fonts/SpaceMono-Regular.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/expo-gh-pages-test/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/expo-gh-pages-test/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/expo-gh-pages-test/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/expo-gh-pages-test/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/expo-gh-pages-test/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/expo-gh-pages-test/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/expo-gh-pages-test/./fonts/AntDesign.ttf"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.832b959655dae6e8c1ea925ec578c433.js"
+  "/expo-gh-pages-test/precache-manifest.8fda497cbc4bda0aa72ca7567952b3a6.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/expo-gh-pages-test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
